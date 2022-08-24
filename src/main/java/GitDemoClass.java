3b0ab0054d2df4f3dfbbbfd034f98a33a89f0d03
priyanka <beatester@gmail.com>
2022-08-24 08:53:26 +0530

    coomit with changes

--- src/main/java/GitDemoClass.java
+++ src/main/java/GitDemoClass.java
@@ -4,6 +4,7 @@ public class GitDemoClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hye");
+		System.out.println("checkout 1");
 		System.out.println("checkout 2");
 		
 	}
